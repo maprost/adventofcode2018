0633a962063936289d9b5624199b262c68279be2
Matthias <matthias.leuthaeuser@gmx.de>
2018-12-03 20:37:49 +0100

    finish task 0301 && 0302

--- day03/task02/go/main.go
+++ day03/task02/go/main.go
@@ -1,10 +1,58 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/maprost/adventofcode2018/golib"
 )
 
-func main() {
-	golib.Read("day03/task02/input.txt")
+const (
+	size    = 1000
+	empty   = 0
+	overlap = -1
+)
 
+func main() {
+	input := golib.Read("day03/task02/input_560.txt")
+
+	// init fabric
+	fabric := make([][]int, size)
+	for i := 0; i < size; i++ {
+		fabric[i] = make([]int, size)
+	}
+
+	// fill fabric
+	for _, in := range input {
+		id, leftEdge, topEdge, wide, height := golib.SplitFabricInput(in)
+
+		for w := leftEdge; w < leftEdge+wide; w++ {
+			for h := topEdge; h < topEdge+height; h++ {
+				fill(fabric, id, w, h)
+			}
+		}
+	}
+
+	// check overlaps
+	for _, in := range input {
+		id, leftEdge, topEdge, wide, height := golib.SplitFabricInput(in)
+		noOverlaps := true
+
+		for w := leftEdge; w < leftEdge+wide; w++ {
+			for h := topEdge; h < topEdge+height; h++ {
+				noOverlaps = noOverlaps && fabric[w][h] == id
+			}
+		}
+
+		if noOverlaps {
+			fmt.Println("No Overlaps: ", id)
+		}
+	}
+}
+
+func fill(fabric [][]int, id int, x int, y int) {
+	if fabric[x][y] != empty {
+		fabric[x][y] = overlap
+	} else {
+		fabric[x][y] = id
+	}
 }
