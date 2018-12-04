c77ef302b385480ef0d7db45649588087246de42
pinguines <spaky3000@gmail.com>
2018-12-04 18:34:40 +0100

    my first commit for python

--- day01/task01/python/main.py
+++ day01/task01/python/main.py
@@ -1,5 +1,10 @@
-
-fobj = open("yellow_snow.txt")
+summe = 0
+s = "numbers.txt"
+fobj = open("numbers.txt")
 for line in fobj:
-    print line.rstrip()
+    print(line)
+    i = int(line)
+    summe = i + summe
+    print(summe)
 fobj.close()
+
