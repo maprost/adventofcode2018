debebff666ac1c402d602c7e468e4c5df2eeac24
pinguines <spaky3000@gmail.com>
2018-12-08 21:26:09 +0100

    my second code for python

---
+++ day01/task02/python/pinguines.py
@@ -0,0 +1,23 @@
+summe = 0
+sums = []
+nums = [+3, +3, +4, -2, -4]
+num = 0
+i = num
+s = "numbers2.txt"
+#fobj = open("numbers2.txt")
+#for line in fobj:
+#    nums.append(int(line))
+#fobj.close()
+while True:
+    for num in nums:
+        print(num)
+    #    i = int(line)
+        summe = summe + num
+        print(summe)
+    #    nums.append(num)
+        for elem in sums:
+            if elem == summe:
+                print(summe)
+                print("ja")
+        sums.append(summe)
+    #fobj.close()
